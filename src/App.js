868fae2d94244b9ea07c94f7253ab35c2dbed004
mukesh-kk <mukeshkushwaha918@gmail.com>
2021-09-13 21:38:37 +0530

    Final done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
 
-
-
-function App() {
-  return (
-    
- <div className="main-grid">
-   <header>
-     <a>
-  SHoPY
-     </a>
-   </header>
-   <main>
-     products
-   </main>
-   <footer>
-     All rights reserved
-   </footer>
- </div>
-
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              <Link to="/">React Shopping Cart</Link>
+              <Link to="/admin">Admin</Link>
+            </header>
+            <main>
+              <Route path="/admin" component={AdminScreen} />
+              <Route path="/" component={HomeScreen} exact />
+            </main>
+            <footer>All right is reserved.</footer>
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
- 
+  <React.StrictMode>
     <App />
- ,
-  document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
